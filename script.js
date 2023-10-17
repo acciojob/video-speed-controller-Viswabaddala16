c6a12d5c8ff6ffa838dd29be786df7407cfc3fd8
Viswabaddala16 <122138862+Viswabaddala16@users.noreply.github.com>
2023-10-17 21:51:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('.viewer');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+const toggle = document.querySelector('.toggle');
+const volume = document.querySelector('input[name="volume"]');
+const playbackRate = document.querySelector('input[name="playbackRate"]');
+const skipButtons = document.querySelectorAll('[data-skip]');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+// Add your existing event listeners here (e.g., togglePlay, updateButton, skip, handleRangeUpdate, handleProgress, and scrub functions)
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+const inputs = document.querySelectorAll('.player__slider');
+
+function handleUpdate() {
+  if (this.name === 'volume') {
+    video.volume = this.value;
+  }
+  if (this.name === 'playbackRate') {
+    video.playbackRate = this.value;
+  }
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
